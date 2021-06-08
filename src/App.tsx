b44fb15a3774e084ce5a41caecb18894be743a18
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2021-06-08 20:58:03 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
+
 export function App() {
   return (
     <div className="App">
      <h1>Hello Wordl</h1>
+     <GlobalStyle />
     </div>
   );
 }
