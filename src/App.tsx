a9ef149584664bb7e220de8d73068649beab0bd2
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2021-06-09 16:12:50 -0300

    update styles and functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
-    <div className="App">
-     <h1>Hello Wordl</h1>
-     <GlobalStyle />
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Dashboard />
+    </>
   );
 }
 
